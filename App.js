1c5108da81de0280a3e63caf1bc91c6e1c49493f
langochai <langochai11121998@gmail.com>
2024-02-19 15:08:04 +0700

    1st commit

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Dimensions} from 'react-native';
+import Constants from 'expo-constants'
+import Box from './components/Box';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Box style={{backgroundColor:"#F94144"}}></Box>
+        <Box style={{backgroundColor:"#90BE6D"}}></Box>
+        <Box style={{backgroundColor:"#577590"}}></Box>
+        <Box style={{backgroundColor:"#F3722C"}}></Box>
+        <Box style={{backgroundColor:"#F9C74F"}}></Box>
     </View>
   );
 }
+const windowSize = Dimensions.get("window")
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    borderWidth: 10,
+    width: windowSize.width,
+    height:windowSize.height,
+    marginTop: Constants.statusBarHeight,
   },
 });

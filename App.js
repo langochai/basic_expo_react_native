4bd174f5415441452b6c9ee620f097c271896edc
langochai <langochai11121998@gmail.com>
2024-02-19 15:26:14 +0700

    basic control of screen orientation

--- App.js
+++ App.js
@@ -1,26 +1,45 @@
-import { StyleSheet, Text, View , Dimensions} from 'react-native';
-import Constants from 'expo-constants'
-import Box from './components/Box';
+import { StyleSheet, Dimensions, ScrollView } from "react-native";
+import Constants from "expo-constants";
+import Box from "./components/Box";
+import { useEffect, useState } from "react";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-        <Box style={{backgroundColor:"#F94144"}}></Box>
-        <Box style={{backgroundColor:"#90BE6D"}}></Box>
-        <Box style={{backgroundColor:"#577590"}}></Box>
-        <Box style={{backgroundColor:"#F3722C"}}></Box>
-        <Box style={{backgroundColor:"#F9C74F"}}></Box>
-    </View>
-  );
+    const [dimension, setDimension] = useState({
+        window: Dimensions.get("window"),
+    });
+    useEffect(() => {
+        const subcription = Dimensions.addEventListener(
+            "change",
+            ({ window }) => {
+                setDimension({ window });
+            }
+        );
+        return () => subcription?.remove();
+    });
+    return (
+        <ScrollView style={styles(dimension.window).container}>
+            <Box style={{ backgroundColor: "#F94144" }}></Box>
+            <Box style={{ backgroundColor: "#90BE6D" }}></Box>
+            <Box style={{ backgroundColor: "#577590" }}></Box>
+            <Box style={{ backgroundColor: "#F3722C" }}></Box>
+            <Box style={{ backgroundColor: "#F9C74F" }}></Box>
+            <Box style={{ backgroundColor: "#F94144" }}></Box>
+            <Box style={{ backgroundColor: "#90BE6D" }}></Box>
+            <Box style={{ backgroundColor: "#577590" }}></Box>
+            <Box style={{ backgroundColor: "#F3722C" }}></Box>
+            <Box style={{ backgroundColor: "#F9C74F" }}></Box>
+        </ScrollView>
+    );
 }
-const windowSize = Dimensions.get("window")
 
-const styles = StyleSheet.create({
-  container: {
-    flex:1,
-    borderWidth: 10,
-    width: windowSize.width,
-    height:windowSize.height,
-    marginTop: Constants.statusBarHeight,
-  },
-});
+const styles = (window) => {
+    return StyleSheet.create({
+        container: {
+            flex: 1,
+            borderWidth: 10,
+            width: window.width,
+            height: window.height,
+            marginTop: Constants.statusBarHeight,
+        },
+    });
+};

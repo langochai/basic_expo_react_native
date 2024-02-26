d9fc1ebf8f418b249b09f4cf928be701f2d6ab1a
langochai <langochai11121998@gmail.com>
2024-02-26 09:54:30 +0700

    starting with bottom tab navigator

--- App.js
+++ App.js
@@ -1,94 +1,19 @@
-import {
-    StyleSheet,
-    Dimensions,
-    ScrollView,
-    View,
-    StatusBar,
-} from "react-native";
-import Constants from "expo-constants";
-import * as ScreenOrientation from "expo-screen-orientation";
-import Box from "./components/Box";
-import { useEffect, useState } from "react";
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import AppNavigator from './app/navigation/AppNavigator'
+import { View , Text, StyleSheet} from 'react-native'
 
 export default function App() {
-    const [dimension, setDimension] = useState({
-        window: Dimensions.get("window"),
-        statusBarHeight: orientation == 1 || orientation == 2 ? 24 : 38,
-    });
-    const [orientation, setOrientation] = useState();
-    const checkOrientation = async () => {
-        const orientation = await ScreenOrientation.getOrientationAsync();
-        setOrientation(orientation);
-    };
-    const handleOrientationChange = (o) => {
-        setOrientation(o.orientationInfo.orientation);
-    };
-    useEffect(() => {
-        checkOrientation();
-        const DimensionChange = Dimensions.addEventListener(
-            "change",
-            ({ window }) => {
-                setDimension({
-                    window,
-                    statusBarHeight:
-                        orientation == 1 || orientation == 2 ? 24 : 38,
-                });
-            }
-        );
-        const OrientationChange =
-            ScreenOrientation.addOrientationChangeListener(
-                handleOrientationChange
-            );
-        return () => {
-            DimensionChange?.remove();
-            ScreenOrientation.removeOrientationChangeListeners(
-                OrientationChange
-            );
-        };
-    }, [orientation]);
-    return (
-        <View
-            style={
-                styles(dimension.window, dimension.statusBarHeight).container
-            }
-        >
-            <ScrollView
-                showsHorizontalScrollIndicator={false}
-                showsVerticalScrollIndicator={false}
-            >
-                <Box style={{ backgroundColor: "#F94144" }}></Box>
-                <Box style={{ backgroundColor: "#90BE6D" }}></Box>
-                <Box style={{ backgroundColor: "#577590" }}></Box>
-                <Box style={{ backgroundColor: "#F3722C" }}></Box>
-                <Box style={{ backgroundColor: "#F9C74F" }}></Box>
-                <Box style={{ backgroundColor: "#F94144" }}></Box>
-                <Box style={{ backgroundColor: "#90BE6D" }}></Box>
-                <Box style={{ backgroundColor: "#577590" }}></Box>
-                <Box style={{ backgroundColor: "#F3722C" }}></Box>
-                <Box style={{ backgroundColor: "#F9C74F" }}></Box>
-                <Box style={{ backgroundColor: "#F94144" }}></Box>
-                <Box style={{ backgroundColor: "#90BE6D" }}></Box>
-                <Box style={{ backgroundColor: "#577590" }}></Box>
-                <Box style={{ backgroundColor: "#F3722C" }}></Box>
-                <Box style={{ backgroundColor: "#F9C74F" }}></Box>
-                <Box style={{ backgroundColor: "#F94144" }}></Box>
-                <Box style={{ backgroundColor: "#90BE6D" }}></Box>
-                <Box style={{ backgroundColor: "#577590" }}></Box>
-                <Box style={{ backgroundColor: "#F3722C" }}></Box>
-                <Box style={{ backgroundColor: "#F9C74F" }}></Box>
-            </ScrollView>
-        </View>
-    );
+  return (
+    <NavigationContainer>
+        <AppNavigator/>
+    </NavigationContainer>
+  )
 }
-
-const styles = (window, statusBarHeight) => {
-    return StyleSheet.create({
-        container: {
-            flex: 1,
-            borderWidth: 10,
-            width: window.width,
-            height: window.height,
-            marginTop: statusBarHeight,
-        },
-    });
-};
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
---
+++ app/screens/Player.js
@@ -0,0 +1,17 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Player() {
+    return (
+        <View style={styles.container}>
+            <Text>Player</Text>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+});
